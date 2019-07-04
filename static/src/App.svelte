8b6ca31fd71d9ee13ecd7fd8b0816734bcfd743b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-04 22:15:11 +0900

    fix App.svelte

--- static/src/App.svelte
+++ static/src/App.svelte
@@ -1,7 +1,18 @@
 <script>
-    let word = "Hello World!"
+    import { Router, Link, Route } from "svelte-routing"
+    import index from "./routes/index.svelte"
+    import about from "./routes/about.svelte"
+    import contact from "./routes/contact.svelte"
+    import Header from "./components/header.svelte"
+    export let url = ""
 </script>
 
-<div data-controller="hello">
-    <h1>{ word }</h1>
-</div>
+<Router url="{url}">
+  <Header />
+  <div>
+    <Route path="/" component="{index}" />
+    <Route path="/about" component="{about}" />
+    <Route path="/contact" component="{contact}" />
+    <Route path="/"><index /></Route>
+  </div>
+</Router>
